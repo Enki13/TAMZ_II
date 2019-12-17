b71cfaf27ef701a91cb57f71f697d8488d6f548e
Dominik Kříž <kriz.dominik13@gmail.com>
2019-12-17 18:53:42 +0100

    Added undo a redo feature and image saving.

--- app/src/main/java/com/example/tamz_project/MainActivity.java
+++ app/src/main/java/com/example/tamz_project/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.tamz_project;
 
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,11 +33,19 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private DrawerLayout drawer;
     private MyDrawView drawView;
+    final MediaPlayer mp = MediaPlayer.create(this, R.raw.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +60,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -64,6 +74,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.undo:
+                drawView.undo();
+                return true;
+            case R.id.redo:
+                drawView.redo();
+                return true;
+            case R.id.action_save:
+                saveImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -82,23 +110,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             case R.id.lower_layer: {
                 drawView.setLayer(1);
+                mp.start();
                 break;
             }
             case R.id.middle_layer: {
                 drawView.setLayer(2);
+                mp.start();
                 break;
             }
             case R.id.upper_layer: {
                 drawView.setLayer(3);
+                mp.start();
                 break;
             }
 
             case R.id.erase_layer: {
                 drawView.eraseLayer();
+                mp.start();
                 break;
             }
             case R.id.erase_all: {
                 drawView.eraseAll();
+                mp.start();
                 break;
             }
 
@@ -151,6 +184,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         yourDialog.cancel();
                     }
                 });
+                mp.start();
                 yourDialog.show();
             }
         }
@@ -163,5 +197,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setCheckedItem(R.id.lower_layer);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    void saveImage() {
+
+        try {
+            Date date = Calendar.getInstance().getTime();
+            DateFormat dateFormat = new SimpleDateFormat("yyyymmdd_hhmmss");
+            String strDate = dateFormat.format(date);
+
+            File dir = new File("/sdcard/drawapp/");
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            File f = new File(dir ,strDate + ".png");
+            f.createNewFile();
+            FileOutputStream out = new FileOutputStream(f);
+            Bitmap bitmap = drawView.getCanvasBitmap();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/tamz_project/MyDrawView.java
+++ app/src/main/java/com/example/tamz_project/MyDrawView.java
@@ -25,13 +25,18 @@ public class MyDrawView extends View {
     private ArrayList<Stroke> layer1 = new ArrayList<Stroke>();
     private ArrayList<Stroke> layer2 = new ArrayList<Stroke>();
     private ArrayList<Stroke> layer3 = new ArrayList<Stroke>();
+    private ArrayList<Stroke> undoLayer1 = new ArrayList<Stroke>();
+    private ArrayList<Stroke> undoLayer2 = new ArrayList<Stroke>();
+    private ArrayList<Stroke> undoLayer3 = new ArrayList<Stroke>();
     private int layer = 1;
+    private int widht;
+    private int height;
 
-    private class Stroke{
+    private class Stroke {
         private Paint paint;
         private Path path;
 
-        public Stroke(Paint paint, Path path){
+        public Stroke(Paint paint, Path path) {
             this.paint = paint;
             this.path = path;
         }
@@ -47,29 +52,33 @@ public class MyDrawView extends View {
 
     public MyDrawView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public MyDrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public MyDrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        widht = w;
+        height = h;
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         drawCanvas = new Canvas(canvasBitmap);
     }
 
-    private void init(){
+    private void init(Context context) {
         brushPath = new Path();
         createNewBrush();
+        widht = context.getResources().getDisplayMetrics().widthPixels;
+        height = context.getResources().getDisplayMetrics().heightPixels;
     }
 
     @Override
@@ -77,12 +86,12 @@ public class MyDrawView extends View {
         for (Stroke s : layer1) {
             canvas.drawPath(s.getPath(), s.getPaint());
         }
-        if(layer > 1){
+        if (layer > 1) {
             for (Stroke s : layer2) {
                 canvas.drawPath(s.getPath(), s.getPaint());
             }
         }
-        if(layer > 2){
+        if (layer > 2) {
             for (Stroke s : layer3) {
                 canvas.drawPath(s.getPath(), s.getPaint());
             }
@@ -95,19 +104,30 @@ public class MyDrawView extends View {
         float x = event.getX();
         float y = event.getY();
 
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 brushPath.reset();
                 brushPath.moveTo(x, y);
                 mX = x;
                 mY = y;
+                switch (layer) {
+                    case 1:
+                        undoLayer1.clear();
+                        break;
+                    case 2:
+                        undoLayer2.clear();
+                        break;
+                    case 3:
+                        undoLayer3.clear();
+                        break;
+                }
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
                 float dx = Math.abs(x - mX);
                 float dy = Math.abs(y - mY);
                 if (dx >= 5 || dy >= 5) {
-                    brushPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                    brushPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
                     mX = x;
                     mY = y;
                 }
@@ -116,10 +136,16 @@ public class MyDrawView extends View {
             case MotionEvent.ACTION_UP:
                 brushPath.lineTo(mX, mY);
                 //drawCanvas.drawPath(brushPath, brushPaint);
-                switch (layer){
-                    case 1 : layer1.add(new Stroke(brushPaint, brushPath)); break;
-                    case 2 : layer2.add(new Stroke(brushPaint, brushPath)); break;
-                    case 3 : layer3.add(new Stroke(brushPaint, brushPath)); break;
+                switch (layer) {
+                    case 1:
+                        layer1.add(new Stroke(brushPaint, brushPath));
+                        break;
+                    case 2:
+                        layer2.add(new Stroke(brushPaint, brushPath));
+                        break;
+                    case 3:
+                        layer3.add(new Stroke(brushPaint, brushPath));
+                        break;
                 }
                 brushPath = new Path();
                 invalidate();
@@ -130,28 +156,34 @@ public class MyDrawView extends View {
         return true;
     }
 
-    public void setLayer(int l){
+    public void setLayer(int l) {
         layer = l;
         invalidate();
     }
 
-    public void eraseLayer(){
-        switch (layer){
-            case 1 : layer1.clear(); break;
-            case 2 : layer2.clear(); break;
-            case 3 : layer3.clear(); break;
+    public void eraseLayer() {
+        switch (layer) {
+            case 1:
+                layer1.clear();
+                break;
+            case 2:
+                layer2.clear();
+                break;
+            case 3:
+                layer3.clear();
+                break;
         }
         invalidate();
     }
 
-    public void eraseAll(){
+    public void eraseAll() {
         layer1.clear();
         layer2.clear();
         layer3.clear();
         invalidate();
     }
 
-    public void createNewBrush(){
+    public void createNewBrush() {
         brushPaint = new Paint();
         brushPaint.setColor(brushColor);
         brushPaint.setAntiAlias(true);
@@ -161,8 +193,56 @@ public class MyDrawView extends View {
         brushPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
-    public void setBrushSize(int size){
+    public void setBrushSize(int size) {
         createNewBrush();
         brushPaint.setStrokeWidth(size);
     }
+
+    public void undo() {
+        switch (layer) {
+            case 1:
+                if (!layer1.isEmpty()) {
+                    undoLayer1.add(layer1.remove(layer1.size() - 1));
+                }
+                break;
+            case 2:
+                if (!layer2.isEmpty()) {
+                    undoLayer2.add(layer2.remove(layer2.size() - 1));
+                }
+                break;
+            case 3:
+                if (!layer3.isEmpty()) {
+                    undoLayer3.add(layer3.remove(layer3.size() - 1));
+                }
+                break;
+        }
+        invalidate();
+    }
+
+    public void redo() {
+        switch (layer) {
+            case 1:
+                if (!undoLayer1.isEmpty()) {
+                    layer1.add(undoLayer1.remove(undoLayer1.size() - 1));
+                }
+                break;
+            case 2:
+                if (!undoLayer2.isEmpty()) {
+                    layer2.add(undoLayer2.remove(undoLayer2.size() - 1));
+                }
+                break;
+            case 3:
+                if (!undoLayer3.isEmpty()) {
+                    layer3.add(undoLayer3.remove(undoLayer3.size() - 1));
+                }
+                break;
+        }
+        invalidate();
+    }
+
+    public Bitmap getCanvasBitmap(){
+        canvasBitmap = Bitmap.createBitmap(widht, height, Bitmap.Config.ARGB_8888);
+        return canvasBitmap;
+    }
+
 }
